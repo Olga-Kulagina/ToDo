07def1871047540602fb09cf67f2b6a07e485edc
Olga-Kulagina <olga.kulagina17@yandex.ru>
2020-10-18 00:12:16 +0300

    Add tasksAPI

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -9,7 +9,7 @@ const instance = axios.create({
 })
 
 
-type TodolistType= {
+type TodolistType = {
     id: string
     addedDate: string
     order: number
@@ -25,11 +25,10 @@ type CommonResponseType<T = {}> = {
 
 export const todolistAPI = {
     getTodolists() {
-        return instance.get<Array<TodolistType>>('todo-lists',)
+        return instance.get<Array<TodolistType>>('todo-lists')
     },
     createTodolist(title: string) {
-        return instance.post<CommonResponseType<{item: TodolistType}>>('todo-lists',
-            {title})
+        return instance.post<CommonResponseType<{ item: TodolistType }>>('todo-lists', {title})
     },
     deleteTodolist(id: string) {
         return instance.delete<CommonResponseType>(`todo-lists/${id}`)
@@ -38,5 +37,44 @@ export const todolistAPI = {
         return instance.put<CommonResponseType>(`todo-lists/${id}`, {title})
 
     }
-
 }
+
+type TaskType = {
+    addedDate: string
+    deadline: string
+    description: string
+    id: string
+    order: number
+    priority: number
+    startDate: string
+    status: number
+    title: string
+    todoListId: string
+}
+
+export const taskAPI = {
+    getTasks(todolistId: string) {
+        return instance.get<Array<TaskType>>(`todo-lists/${todolistId}/tasks`)
+    },
+    createTask(todolistId: string, title: string) {
+        return instance.post<CommonResponseType<{ item: TaskType }>>(`todo-lists/${todolistId}/tasks`, {title})
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete<CommonResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+    updateTask(todolistId: string, taskId: string, title: string, description: string,
+    completed: string, status: number, priority: number, startDate: string, deadline: string) {
+        return instance.put<CommonResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`, {
+            title,
+            description,
+            completed,
+            status,
+            priority,
+            startDate,
+            deadline
+        })
+    }
+}
+
+
+
